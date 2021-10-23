b187d3ff074c5e049c04e96eb244afb4e93c6d8e
Lovro <obreza.lovro@gmail.com>
2021-10-23 18:44:16 +0200

    auth

--- app.js
+++ app.js
@@ -7,13 +7,17 @@ const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
 const session = require('express-session')
 const flash = require('connect-flash')
-const Dog = require('./models/dogs')
+const passport = require('passport')
+const passportLocal = require('passport-local')
 
+const Dog = require('./models/dogs')
+const User = require('./models/user')
 
 const catchAsync = require('./public/javascript/CatchAsync')
 const ExpressError = require('./public/javascript/ExpressError')
 
 const validateDog = require('./public/javascript/Joi')
+
 //mongose connection
 mongoose.connect('mongodb://localhost:27017/posvoji', {
     useNewUrlParser: true,
@@ -26,7 +30,7 @@ db.once('open', ()=>{
     console.log('database connected');
 })
 
-//middleware and express set
+//middleware
 app.use(express.static('public'))
 
 app.engine('ejs', ejsMate)
@@ -37,8 +41,6 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 app.use(methodOverride('_method'))
-
-// flash
 
 //sessions
 const sessionConfig={
@@ -63,6 +65,13 @@ app.use((req,res,next)=>{
     next()
 })
 
+//passport authentication
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new passportLocal(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 //routes
 app.get('/', (req,res)=>{
@@ -76,14 +85,18 @@ app.get('/dogs', catchAsync(async(req,res) => {
 }))
 
 //POST NEW DOG
-app.get('/dogs/new', (req,res)=>{
+app.get('/new', (req,res)=>{
+    if(!req.isAuthenticated()){
+        req.flash('error', 'You must sign in first')
+        res.redirect('/login')
+    }
     res.render('dogs/new')
 })
 
 app.post('/dogs', validateDog, catchAsync(async(req,res, next)=>{
     const dog = new Dog(req.body.dog)
     await dog.save()
-    req.flash('success', 'Successfully publish your puppy')
+    req.flash('success', 'Successfully published your puppy')
     res.redirect(`/dogs/${dog._id}`)
 }))
 
@@ -91,6 +104,10 @@ app.post('/dogs', validateDog, catchAsync(async(req,res, next)=>{
 app.get('/dogs/:id', catchAsync(async(req,res) => {
     const {id} = req.params
     const dog = await Dog.findById(id)
+    if(!dog){
+        req.flash('error', 'Thankfuly the puppy already found a new home. Check out some others in need of love.')
+        return res.redirect('/dogs')
+    }
     res.render('dogs/details', { dog })
 }))
 
@@ -98,7 +115,10 @@ app.get('/dogs/:id', catchAsync(async(req,res) => {
 app.get('/dogs/:id/edit', catchAsync(async(req,res) => {
     const {id} = req.params
     const dog = await Dog.findById(id)
-    
+    if(!dog){
+        req.flash('error', 'Thankfuly the puppy already found a new home. Check out some others in need of love.')
+        return res.redirect('/dogs')
+    }
     res.render('dogs/edit', { dog })
 }))
 
@@ -107,6 +127,7 @@ app.put('/dogs/:id', validateDog, catchAsync(async(req,res)=>{
     const {id} = req.params
     const updatedDog = await Dog.findByIdAndUpdate(id, {...req.body.dog})
     updatedDog.save()
+    req.flash('success', 'Successfully updated your puppys information')
     res.redirect(`/dogs/${updatedDog._id}`)
 }))
 
@@ -114,9 +135,38 @@ app.put('/dogs/:id', validateDog, catchAsync(async(req,res)=>{
 app.delete('/dogs/:id', catchAsync(async(req,res)=>{
     const {id} = req.params
     await Dog.findByIdAndDelete(id)
+    req.flash('success', 'Thank you for getting a puppy a new home!')
     res.redirect('/dogs') 
 }))
 
+//register and login
+app.get('/register', (req,res)=>{
+    res.render('passport/register')
+})
+
+app.post('/register', catchAsync(async(req,res)=>{
+    try {
+    const {email,username,password} = req.body 
+    const user = new User({email, username})
+    await User.register(user, password)
+    req.flash('success', 'Wellcome to Posvoji.si')
+    res.redirect('/')    
+} catch(e){
+        req.flash('error', e.message)
+        res.redirect('/register')
+    }
+}))
+
+app.get('/login', (req,res)=>{
+    res.render('passport/login')
+})
+
+app.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res)=>{
+    req.flash('success', 'Welcome back!')
+    res.redirect('/')
+})
+
+// if nothing is found
 app.all('*', (req,res,next) =>{
     next(new ExpressError('Page not found', 404))
 })
