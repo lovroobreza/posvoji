f450778fd32ef91a60d7273dc24e554aa4bd7428
Lovro <obreza.lovro@gmail.com>
2021-10-23 11:21:03 +0200

    errors and flash

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ const path = require('path')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
+const session = require('express-session')
+const flash = require('connect-flash')
 const Dog = require('./models/dogs')
 
+
+const catchAsync = require('./public/javascript/CatchAsync')
+const ExpressError = require('./public/javascript/ExpressError')
+
+const validateDog = require('./public/javascript/Joi')
 //mongose connection
 mongoose.connect('mongodb://localhost:27017/posvoji', {
     useNewUrlParser: true,
@@ -31,6 +38,31 @@ app.use(express.json())
 
 app.use(methodOverride('_method'))
 
+// flash
+
+//sessions
+const sessionConfig={
+    secret: 'hay',
+    resave:false,
+    saveUninitialized: true,
+    cookie:{
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 *7,
+        maxAge: 1000 * 60 * 60 * 24 *7,
+    }
+}
+
+app.use(session( sessionConfig))
+
+//flash
+app.use(flash())
+
+app.use((req,res,next)=>{
+    res.locals.success = req.flash('success')
+    res.locals.error = req.flash('error')
+    next()
+})
+
 
 //routes
 app.get('/', (req,res)=>{
@@ -38,49 +70,61 @@ app.get('/', (req,res)=>{
 })
 
 //show all dogs
-app.get('/dogs', async(req,res) => {
+app.get('/dogs', catchAsync(async(req,res) => {
     const dogs = await Dog.find({})
     res.render('dogs/index', { dogs })
-})
+}))
 
 //POST NEW DOG
 app.get('/dogs/new', (req,res)=>{
     res.render('dogs/new')
 })
 
-app.post('/dogs', async(req,res)=>{
+app.post('/dogs', validateDog, catchAsync(async(req,res, next)=>{
     const dog = new Dog(req.body.dog)
     await dog.save()
+    req.flash('success', 'Successfully publish your puppy')
     res.redirect(`/dogs/${dog._id}`)
-})
+}))
 
 //SHOW SINGLE DOG
-app.get('/dogs/:id', async(req,res) => {
+app.get('/dogs/:id', catchAsync(async(req,res) => {
     const {id} = req.params
     const dog = await Dog.findById(id)
     res.render('dogs/details', { dog })
-})
+}))
 
 //EDIT SINGLE DOG
-app.get('/dogs/:id/edit', async(req,res) => {
+app.get('/dogs/:id/edit', catchAsync(async(req,res) => {
     const {id} = req.params
     const dog = await Dog.findById(id)
     
     res.render('dogs/edit', { dog })
-})
+}))
 
-app.put('/dogs/:id', async(req,res)=>{ 
+app.put('/dogs/:id', validateDog, catchAsync(async(req,res)=>{ 
+    if(!req.body.dog) throw new ExpressError('Invalid try buddy', 400)
     const {id} = req.params
     const updatedDog = await Dog.findByIdAndUpdate(id, {...req.body.dog})
     updatedDog.save()
     res.redirect(`/dogs/${updatedDog._id}`)
-})
+}))
 
 //delete a doggie = succesfully adopted
-app.delete('/dogs/:id', async(req,res)=>{
+app.delete('/dogs/:id', catchAsync(async(req,res)=>{
     const {id} = req.params
     await Dog.findByIdAndDelete(id)
     res.redirect('/dogs') 
+}))
+
+app.all('*', (req,res,next) =>{
+    next(new ExpressError('Page not found', 404))
+})
+
+app.use((err,req,res,next) => {
+    const { statusCode=500} = err
+    if(!err.message) err.message = "something went wrong"
+    res.status(statusCode).render('error/error', {err})
 })
 
 app.listen(3000, () => console.log('Listening on port: 3000'))
---
+++ public/javascript/Joi.js
@@ -0,0 +1,22 @@
+/* JOI VALIDATION */
+const Joi = require('joi')
+const ExpressError = require('./ExpressError')
+
+
+module.exports =  validateDog = (req,res,next) =>{
+    const dogSchema = Joi.object({
+    dog: Joi.object({
+        name: Joi.string().required(),
+        age: Joi.number().required().min(0).max(20),
+        breed: Joi.string().required(),
+        description: Joi.string().required(),
+    }).required()
+})
+    const { error } = dogSchema.validate(req.body)
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else{
+        next()
+    }
+}
